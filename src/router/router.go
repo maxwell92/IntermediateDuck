d4b6047f66c7e6c2a8c99fa8cb9e5b34ffbbaa0c
maxwell92 <710028463@qq.com>
2017-06-05 17:44:42 +0800

    add options

--- src/router/router.go
+++ src/router/router.go
@@ -36,7 +36,7 @@ func (r Router) Handle(w http.ResponseWriter, req *http.Request) {
 
 func (r Router) Options(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Access-Controll-Allow-Origin", "*")
-	w.Header().Set("Access-Controll-Allow-Methods", "GET,POST")
+	w.Header().Set("Access-Controll-Allow-Methods", "GET,POST,OPTIONS")
 	w.Header().Set("Access-Controll-Allow-Headers", "authorization,cache-control,orgid,pragma,userid")
 
 	fmt.Fprintln(w, "")
